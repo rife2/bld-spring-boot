dcbfe4a7e20c5c809f59e44bf2cea3255e8a4fe7
Erik C. Thauvin <erik@thauvin.net>
2024-05-09 21:30:06 -0700

    Bumped bld to version 1.9.1

--- src/bld/java/rife/bld/extension/SpringBootBuild.java
+++ src/bld/java/rife/bld/extension/SpringBootBuild.java
@@ -43,7 +43,7 @@ public class SpringBootBuild extends Project {
         repositories = List.of(MAVEN_LOCAL, MAVEN_CENTRAL, RIFE2_RELEASES, RIFE2_SNAPSHOTS);
 
         scope(compile)
-                .include(dependency("com.uwyn.rife2", "bld", version(1, 9, 0)));
+                .include(dependency("com.uwyn.rife2", "bld", version(1, 9, 1)));
         scope(test)
                 .include(dependency("org.junit.jupiter", "junit-jupiter", version(5, 10, 2)))
                 .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1, 10, 2)))
