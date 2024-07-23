61a40e0c7d9c343f166d190e58dbe1050ddc30b5
Erik C. Thauvin <erik@thauvin.net>
2024-07-22 21:15:35 -0700

    Bumped bld to version 2.0.0-SNAPSHOT

--- src/bld/java/rife/bld/extension/SpringBootBuild.java
+++ src/bld/java/rife/bld/extension/SpringBootBuild.java
@@ -33,7 +33,7 @@ public class SpringBootBuild extends Project {
     public SpringBootBuild() {
         pkg = "rife.bld.extension";
         name = "bld-spring-boot";
-        version = version(0, 9, 6);
+        version = version(0, 9, 7, "SNAPSHOT");
 
         javaRelease = 17;
 
--- src/test/java/rife/bld/extension/BootJarOperationTest.java
+++ src/test/java/rife/bld/extension/BootJarOperationTest.java
@@ -33,7 +33,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 
 class BootJarOperationTest {
-    private static final String BLD = "bld-1.9.1.jar";
+    private static final String BLD = "bld-2.0.0-20240720.190338-27.jar";
     private static final String BOOT_VERSION = "3.3.2";
     private static final String EXAMPLES_LIB_COMPILE = "examples/lib/compile/";
     private static final String EXAMPLES_LIB_RUNTIME = "examples/lib/runtime/";
