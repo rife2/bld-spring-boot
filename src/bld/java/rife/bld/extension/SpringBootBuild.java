035c2f9ef2c37cfcbd5fb4fe1e0bad58e95d846a
Erik C. Thauvin <erik@thauvin.net>
2025-01-14 12:16:08 -0800

    Version 1.0.2

--- src/bld/java/rife/bld/extension/SpringBootBuild.java
+++ src/bld/java/rife/bld/extension/SpringBootBuild.java
@@ -33,7 +33,7 @@ public class SpringBootBuild extends Project {
     public SpringBootBuild() {
         pkg = "rife.bld.extension";
         name = "bld-spring-boot";
-        version = version(1, 0, 1);
+        version = version(1, 0, 2);
 
         javaRelease = 17;
 
