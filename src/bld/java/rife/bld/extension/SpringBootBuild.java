ffb8a706d3c416287afc84773867471f50cf59d5
Erik C. Thauvin <erik@thauvin.net>
2025-01-23 08:56:24 -0800

    Bumped AssertJ to version 3.27.3

--- src/bld/java/rife/bld/extension/SpringBootBuild.java
+++ src/bld/java/rife/bld/extension/SpringBootBuild.java
@@ -47,7 +47,7 @@ public class SpringBootBuild extends Project {
         scope(test)
                 .include(dependency("org.junit.jupiter", "junit-jupiter", version(5, 11, 4)))
                 .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1, 11, 4)))
-                .include(dependency("org.assertj", "assertj-core", version(3, 27, 2)));
+                .include(dependency("org.assertj", "assertj-core", version(3, 27, 3)));
 
         javadocOperation()
                 .javadocOptions()
