6bec436b11698dffc8c168dd2b0b29e309714dd5
Erik C. Thauvin <erik@thauvin.net>
2025-04-24 11:24:42 -0700

    1.0.4-SNAPSHOT

--- src/bld/java/rife/bld/extension/SpringBootBuild.java
+++ src/bld/java/rife/bld/extension/SpringBootBuild.java
@@ -33,7 +33,7 @@ public class SpringBootBuild extends Project {
     public SpringBootBuild() {
         pkg = "rife.bld.extension";
         name = "bld-spring-boot";
-        version = version(1, 0, 3);
+        version = version(1, 0, 4, "SNAPSHOT");
 
         javaRelease = 17;
 
