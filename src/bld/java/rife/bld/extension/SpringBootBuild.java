ab4097dd9018c837454777c6040085b5502ecbdb
Erik C. Thauvin <erik@thauvin.net>
2025-07-11 23:11:15 -0700

    Bump bld to version 2.3.0

--- src/bld/java/rife/bld/extension/SpringBootBuild.java
+++ src/bld/java/rife/bld/extension/SpringBootBuild.java
@@ -46,7 +46,7 @@ public class SpringBootBuild extends Project {
         repositories = List.of(MAVEN_LOCAL, MAVEN_CENTRAL, RIFE2_RELEASES, RIFE2_SNAPSHOTS);
 
         scope(compile)
-                .include(dependency("com.uwyn.rife2", "bld", version(2, 2, 1)));
+                .include(dependency("com.uwyn.rife2", "bld", version(2, 3, 0)));
         scope(test)
                 .include(dependency("org.junit.jupiter", "junit-jupiter", version(5, 13, 2)))
                 .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1, 13, 2)))
