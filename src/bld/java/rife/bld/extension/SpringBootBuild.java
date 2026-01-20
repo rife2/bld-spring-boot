ea4f1b9d5475d9238d738162ae62b40177bd131e
Erik C. Thauvin <erik@thauvin.net>
2026-01-20 14:24:37 -0800

    Use IOUtils to resolve test results directory

--- src/bld/java/rife/bld/extension/SpringBootBuild.java
+++ src/bld/java/rife/bld/extension/SpringBootBuild.java
@@ -18,11 +18,11 @@ package rife.bld.extension;
 
 import rife.bld.BuildCommand;
 import rife.bld.Project;
+import rife.bld.extension.tools.IOUtils;
 import rife.bld.publish.PublishDeveloper;
 import rife.bld.publish.PublishLicense;
 import rife.bld.publish.PublishScm;
 
-import java.io.File;
 import java.util.List;
 
 import static rife.bld.dependencies.Repository.*;
@@ -94,9 +94,8 @@ public class SpringBootBuild extends Project {
 
     @Override
     public void test() throws Exception {
-        var testResultsDir = "build/test-results/test/";
         var op = testOperation().fromProject(this);
-        op.testToolOptions().reportsDir(new File(testResultsDir));
+        op.testToolOptions().reportsDir(IOUtils.resolveFile(buildDirectory(), "test-results", "test"));
         op.execute();
     }
 
