cc0763468ecc161f5a9a5bb5a67462f75481f1d8
Erik C. Thauvin <erik@thauvin.net>
2026-02-19 19:20:56 -0800

    Bump Testing Helpers to version 0.9.6

--- src/bld/java/rife/bld/extension/SpringBootBuild.java
+++ src/bld/java/rife/bld/extension/SpringBootBuild.java
@@ -54,7 +54,7 @@ public class SpringBootBuild extends Project {
                         version(4, 9, 8)));
         scope(test)
                 .include(dependency("com.uwyn.rife2", "bld-extensions-testing-helpers",
-                        version(0, 9, 6, "SNAPSHOT")))
+                        version(0, 9, 6)))
                 .include(dependency("org.junit.jupiter", "junit-jupiter", junit))
                 .include(dependency("org.junit.platform", "junit-platform-console-standalone", junit))
                 .include(dependency("org.assertj", "assertj-core",
